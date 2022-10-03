8bb1623f94008d560ea3f1519eddf606848c9dab
DaveJohno <88128050+DaveJohno@users.noreply.github.com>
2022-10-03 19:55:09 +1100

    landing page styling done

--- src/Componants/Container/Container.js
+++ src/Componants/Container/Container.js
@@ -1,5 +1,24 @@
 import "./Container.scss";
 
 export default function Container() {
-  return <main className="Container"></main>;
+  return (
+    <main className="Container">
+      <header className="">
+        <h1 className>
+          Imagine if <br />
+          <span className="snap-chat">Snapchat</span>
+          <br /> had events.
+        </h1>
+
+        <p className="">
+          Easily host and share events with your friends across any social
+          media.
+        </p>
+      </header>
+
+      <div className="image-one"></div>
+
+      <button className="create-btn">🎉 Create my event</button>
+    </main>
+  );
 }
